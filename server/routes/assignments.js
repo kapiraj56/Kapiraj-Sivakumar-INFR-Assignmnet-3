ff1f2313f88c7718b8902045ffe8abb048c4a5b0
kapiraj56 <160338459+kapiraj56@users.noreply.github.com>
2024-11-24 00:15:37 -0500

    updated

--- server/routes/assignments.js
+++ server/routes/assignments.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var router = express.Router();
-let Book = require('../model/book')
-router.get('/',async(req,res,next)=>{
-    try{
-        const BookList = await Book.find();
-        res.render('book',{
-            title:'Books',
-            BookList:BookList
-        })
-    }
-    catch(err){
-        console.error(err)
-        res.render('book',{
-            error:'Error on Server'})
-    }
-})
-module.exports = router;
+//const { router } = require('../config/app');
+let Assignment = require('../models/Assignment_trackers');
+let AssignmentController = require('../controllers/Assignment_trackers')
+/* Get route for the Bio Assignments list */
+// Read Operation
+router.get('/', AssignmentController.DislayAssignmentlist);
+/* Get route for Add Assignment page --> Create */
+router.get('/add', AssignmentController.AddAssignment); 
+/* Post route for Add Assignment page --> Create */
+router.post('/add', AssignmentController.ProcessAssignment);
+/* Get route for displaying the Edit Assignment page --> Update */
+router.get('/edit/:id', AssignmentController.EditAssignment);
+/* Post route for processing the Edit Assignment page --> Update */
+router.post('/edit/:id', AssignmentController.ProcessEditAssignment);
+/* Get to perform Delete Operation --> Delete Operation */
+router.get('/delete/:id', AssignmentController.DeleteAssignment);
+ module.exports = router;
